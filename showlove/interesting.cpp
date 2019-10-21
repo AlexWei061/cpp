5b2d1a949c3fff805f02d4e5a853b8f17e500240
AlexWei <ty.wei@foxmail.com>
2019-10-21 21:33:35 +0800

    my friend's code

---
+++ showlove/interesting.cpp
@@ -0,0 +1,4 @@
+#include "stdc++.h"
+using namespace jsp;
+I love you xxx even though nobody support us
+So let me care about you anywhere my dear 
---
+++ showlove/stdc++.h
@@ -0,0 +1,23 @@
+#ifndef STDC++_H
+#define STDC++_H 
+#include <iostream>
+namespace jsp{
+	#define I int main()
+	#define love {
+	#define you std::ios::sync_with_stdio(false);
+	#define xxx std::string a,b,c,d;
+	#define even a="love ",c="I ",b="you",d="|";
+	#define though printf("\n\n\n\n\n\n\n\n                                                 *****   *****\n"
+	#define nobody "                                                ******* *******\n"
+	#define support "                                               *****************\n"
+	#define us "                                                ***************\n"
+	#define So "                                                 *************\n"
+	#define let "                                                  ***********\n"
+	#define me "                                                    *******\n"
+	#define care "                                                       *\n");
+	#define about std::cout<<"                                                 "<<d<<c<<a<<b<<d<<std::endl;
+	#define anywhere ;
+	#define my return 0;
+	#define dear }
+}
+#endif
