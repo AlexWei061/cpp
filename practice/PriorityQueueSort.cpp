62b3f426135db165f23309cee8511e1e92040c27
AlexWei <ty.wei@foxmail.com>
2019-10-26 20:30:29 +0800

    complete my PriorityQueueSort

--- practice/PriorityQueueSort.cpp
+++ practice/PriorityQueueSort.cpp
@@ -2,18 +2,19 @@
 #include<queue>
 using namespace std;
 
+priority_queue<int> NumList;
+int num_list[1000001];
+
 void PriorityQueueSort(int n, int num_list[]){
-    priority_queue<int> NumList;
     for (int i = 0; i < n; i++){
         NumList.push(num_list[i]);
     }
     for (int j = 0; j < n; j++){
-        printf("%d%c",NumList.top(),' ');
+        num_list[j] = NumList.top();
         NumList.pop();
     }
 }
 
-int num_list[1000001];
 int n = 0;
 
 int main(){
@@ -22,5 +23,8 @@ int main(){
         scanf("%d",&num_list[i]);
     }
     PriorityQueueSort(n, num_list);
+    for (int j = 0; j < n; j++){
+    	printf("%d%c",num_list[j],' ');
+    }
     return 0;
 }
