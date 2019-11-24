44c13e3ab8f88750112773da13da646a5eaa4b87
AlexWei <ty.wei@foxmail.com>
2019-11-24 09:43:52 +0800

    corect it

--- giraffe/giraffe.cpp
+++ giraffe/giraffe.cpp
@@ -2,32 +2,9 @@
 #include "giraffe.h"
 using namespace std;
 
-//TODO
-
-/*
-void print(){}
-template<typename T, typename ...Types>
-void print(const T arg, const Types& ...args){
-	cout << arg;
-	print(args...);
-}
-
-void input(){}
-template<typename T, typename ...Types>
-void input(T arg, Types& ...args){
-	cin >> arg;
-	input(args...);
-}
-*/
-
-int ee;
-
 int main(){
     giraffe<int> a;
-    cin >> ee;
-    for(int i = 0; i < ee; i++){
-    	a[i] = i;
-    	cout << a[i] << " ";
-	}
+    a[0] = 1;
+    cout << a[0];
     return 0;
-	}
+}
