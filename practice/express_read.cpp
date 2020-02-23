4e6dd36b65519603b32c3cfd48678f9e21a0e515
AlexWei <ty.wei@foxmail.com>
2020-02-23 19:44:28 +0800

    express read

---
+++ practice/express_read.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+inline int read(){
+   int s=0,w=1;
+   char ch=getchar();
+   while(ch<'0'||ch>'9'){if(ch=='-')w=-1;ch=getchar();}
+   while(ch>='0'&&ch<='9') s=s*10+ch-'0',ch=getchar();
+   return s*w;
+}
+
+int main(){
+	int x = read();
+	printf("%d", x);
+	return 0;
+}
