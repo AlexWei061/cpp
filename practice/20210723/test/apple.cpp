4de8f41d4c03d869443ccbae607e961eb50bcd5e
AlexWei <ty.wei@foxmail.com>
2021-07-23 14:20:13 +0800

    correct some mistakes

--- practice/20210723/test/apple.cpp
+++ practice/20210723/test/apple.cpp
@@ -5,8 +5,8 @@ long long n = 0;
 long long a[100001] = { 0 };
 
 int main(){
-	freopen("apple.in", "r", stdin);
-	freopen("apple.out", "w", stdout);
+	//freopen("apple.in", "r", stdin);
+	//freopen("apple.out", "w", stdout);
 	scanf("%lld", &n);
 	int j = 1;
 	for(int i = 1; i <= sqrt(n); i++){
@@ -17,11 +17,14 @@ int main(){
 	}
 	
 	for(int i = 1; i < j; i++){
-		printf("%d\n", a[i]);
+		printf("%lld\n", a[i]);
 	}
 	
 	for(int i = j-1; i >= 1; i--){
-		printf("%d\n", (int)n/a[i]);
+		if((int)n/a[i] == a[i]){
+			continue;
+		}
+		printf("%lld\n", (int)n/a[i]);
 	}
 	
 	return 0;
