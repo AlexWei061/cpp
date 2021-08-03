3a3b2e505f01e3a63f22e3c254e79a9787834be5
AlexWei <ty.wei@foxmail.com>
2021-08-03 20:38:22 +0800

    add the discriptions

--- practice/2021SummerVacation/August2021/20210803/inverseModulo.cpp
+++ practice/2021SummerVacation/August2021/20210803/inverseModulo.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 ll a, b;
 
+/*
+
+ax ≡ 1 (mod b) --> ax mod b = 1 mod b --> (ax-1) mod b = 0
+b|(ax-1)   设 ax-1 = kb --> ax - kb = 1 令 y = -k 得 ax + by = 1
+gcd(a, b) = 1 --> ax + by = gcd(a, b)
+用扩展欧几里得算法求 x, y
+
+*/
+
 int exgcd(ll a, ll b, ll &x, ll &y){
     if(b == 0){
         x = 1;
