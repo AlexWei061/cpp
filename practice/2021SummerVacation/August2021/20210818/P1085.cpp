aa502e91446494704f9b1ec732d7ed9376abe156
alex.wei <ty.wei@foxmail.com>
2021-08-18 19:55:04 +0800

    add the solution of P1085 in luogu

--- practice/2021SummerVacation/August2021/20210818/P1085.cpp
+++ practice/2021SummerVacation/August2021/20210818/P1085.cpp
@@ -1,7 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-
-    return 0;
+int main (){
+    int a, b, s, max = 0, i, day = 0;
+    for(i = 1; i < 8; i++){
+        cin >> a >> b; 
+        s = a + b;
+        if((s > max) and (s > 8)) max = s, day = i; 
+    }
+    cout << day << endl; 
+    return 0;             
 }
