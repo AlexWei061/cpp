012253c3b95da4c123aac3e70b12ae1106842aed
alex.wei <ty.wei@foxmail.com>
2021-08-20 13:48:50 +0800

    add the solution of P1075 in luogu

--- practice/2021SummerVacation/August2021/20210819/P1075.cpp
+++ practice/2021SummerVacation/August2021/20210819/P1075.cpp
@@ -1,20 +1,20 @@
-#include<bits/stdc++.h>
-using namespace std;
+    #include<bits/stdc++.h>
+    using namespace std;
 
-int n = 0;
+    int n = 0;
 
-int main(){
-    scanf("%d", &n);
-    for(int i = 2; i < sqrt(n); i++){
-        if(n % i == 0){
-            printf("%d\n", n / i);
+    int main(){
+        scanf("%d", &n);
+        for(int i = 2; i < sqrt(n); i++){
+            if(n % i == 0){
+                printf("%d\n", n / i);
+            }
         }
+        return 0;
     }
-    return 0;
-}
 
-/*
+    /*
 
-已知正整数 n 是两个不同的质数的乘积，试求出两者中较大的那个质数。
+    已知正整数 n 是两个不同的质数的乘积，试求出两者中较大的那个质数。
 
-*/
+    */
