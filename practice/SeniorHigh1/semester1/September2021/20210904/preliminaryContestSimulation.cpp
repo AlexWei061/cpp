d9e099c78a7ce3119251ec60240a90522e3187ac
alex.wei <ty.wei@foxmail.com>
2021-09-04 11:48:57 +0800

    add today's contest simulation

---
+++ practice/SeniorHigh1/semester1/September2021/20210904/preliminaryContestSimulation.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    string ans =
+        "ACABBDADCDCABBD"
+        "TFTFBD"
+        "TTTTCC"
+        "TTTTCC"
+        "CCCCC"
+        "CCCCC";
+    cout<<ans<<endl;
+    return 0;
+}
