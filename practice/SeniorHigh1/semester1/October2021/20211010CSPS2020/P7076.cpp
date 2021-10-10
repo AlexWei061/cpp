52652c35834b53ec8155eb0c1a3c101382e0445c
alex.wei <ty.wei@foxmail.com>
2021-10-10 14:26:42 +0800

    avoid overflow

--- practice/SeniorHigh1/semester1/October2021/20211010CSPS2020/P7076.cpp
+++ practice/SeniorHigh1/semester1/October2021/20211010CSPS2020/P7076.cpp
@@ -1,17 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef unsigned long long lluu;
+typedef unsigned long long ull;
 
 int n = 0; int m = 0;
 int c = 0; int k = 0;
 
-lluu a = 0;
-bool rule[70] = { 0 };
+ull a = 0;
+bool rule[100] = { 0 };
 
 int main(){
 	scanf("%d%d%d%d", &n, &m, &c, &k);
 	for(int i = 0; i < n; i++){
-		lluu temp = 0;
+		ull temp = 0;
 		scanf("%llu", &temp);
 		a |= temp;
 	}
@@ -23,21 +23,20 @@ int main(){
 		rule[p] = true;
 	}
 
-	int num = 0;
 	int cnt = 0;
-	for(int i = 0; i <= 64; i++){
-		if(rule[i]){ 
+	for(int i = 0; i < k; i++){
+		if(!rule[i] or ((a >> i) & 1)){ 
 			cnt++;
-			if((a >> i) & 1){
-				num++;
-			}
 		}
 	}
 
-	lluu ans = 1 << (k - cnt + num);
-	ans -= n;
-
-	printf("%d\n", ans);
+	if(cnt == 64 and !n){
+		puts("18446744073709551616");
+	}
+	else{
+		ull ans = (cnt == 64) ? ull(-n) : ull((ull(1) << ull(cnt)) - n);
+		printf("%llu\n", ans);
+	}
 
 	return 0;
 }
