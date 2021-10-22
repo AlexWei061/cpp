c7b2796ee777abfce86233f4b574ac69e4c95301
alex.wei <ty.wei@foxmail.com>
2021-10-22 19:02:04 +0800

    add kruskal

---
+++ practice/SeniorHigh1/semester1/October2021/20211022Templates/kruskal.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAXN 5050
+#define MAXM 200200
+
+// P3366
+
+int n = 0; int m = 0;
+
+struct Tedge{
+	int x, y;
+	int val;
+}edge[MAXM];
+bool comp(Tedge a, Tedge b){
+	return a.val < b.val;
+}
+
+int parent[MAXN] = { 0 };
+
+void init(){
+	for(int i = 1; i <= n; i++){
+		parent[i] = i;
+	}
+}
+
+int findRoot(int x){
+	if(x == parent[x]){
+		return x;
+	}
+	return parent[x] = findRoot(parent[x]);
+}
+
+void merge(int x, int y){
+	parent[findRoot(x)] = findRoot(y);
+}
+
+int ans = 0;
+void kruskal(){
+	sort(edge+1, edge+m+1, comp);
+	for(int i = 1; i <= m; i++){
+		Tedge e = edge[i];
+		if(findRoot(e.x) != findRoot(e.y)){
+			merge(e.x, e.y);
+			ans += e.val;
+		}
+	}
+}
+
+int main(){
+	scanf("%d%d", &n, &m);
+	init();
+	for(int i = 1; i <= m; i++){
+		scanf("%d%d%d", &edge[i].x, &edge[i].y, &edge[i].val);
+		merge(edge[i].x, edge[i].y);
+	}
+	
+	int cnt = 0;
+	for(int i = 1; i <= n; i++){
+		if(parent[i] == i) cnt++;
+	}
+	if(cnt > 1){
+		printf("orz\n");
+	}
+	else{
+		init();
+		kruskal();
+		printf("%d\n", ans);
+	}
+	return 0;
+}
