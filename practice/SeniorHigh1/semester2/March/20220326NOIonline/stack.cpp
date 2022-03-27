8c0c9c28fe4409287b1996df58fd427218e6615f
alex.wei <ty.wei@foxmail.com>
2022-03-27 22:45:15 +0800

    add stack

--- practice/SeniorHigh1/semester2/March/20220326NOIonline/stack.cpp
+++ practice/SeniorHigh1/semester2/March/20220326NOIonline/stack.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 #define in read()
 #define MAXN 500500
-#define MAXM 5050
 
 inline int read(){
 	int x = 0; char c = getchar();
@@ -14,6 +13,8 @@ inline int read(){
 }
 
 int n = 0; int q = 0;
+int top = 0;
+int stk[MAXN] = { 0 };
 struct Tnode{
 	int a; int b;
 	bool operator < (const Tnode &x) const { return (a != x.a) and (b < x.b); }       // 能入栈的条件 
@@ -21,57 +22,53 @@ struct Tnode{
 	bool operator == (const Tnode &x) const { return a == x.a; }
 }arr[MAXN];
 
-int ans[MAXM][MAXM] = { 0 };
-int top = 0;
-int stk[MAXN] = { 0 };
-void solve1(){
-	for(int i = 1; i <= n; top = 0, i++){
-		ans[i][i] = 1; stk[++top] = i;
-		for(int j = i + 1; j <= n; j++){
-			while(top and arr[j] >= arr[stk[top]]) top--;
-			ans[i][j] = ans[i][j - 1] + (top == 0); stk[++top] = j;
-		}
-	}
-	while(q--) { int l = in; int r = in; cout << ans[l][r] << '\n'; }
-}
-
 struct BIT{
+	int t[MAXN << 2];
 	
-	int f[MAXN];
-	vector<int> t[MAXN];
-
-	int lowbit(int x) { return x & -x; }
-	
-	void add(int x){
-		for(int i = x; i <= n; i += lowbit(i))
-			t[i].push_back(f[x]);
+	inline int lowbit(int x) { return x & -x; }
+	void clear() { memset(t, 0, sizeof(t)); }
+	void add(int x, int val){
+		for(; x <= n; x += lowbit(x)) t[x] += val;
 	}
-	
-	int query(int c, int x){
+	int query(int x){
 		int ans = 0;
-		for(int i = x; i; i -= lowbit(i))
-			ans += (lower_bound(t[i].begin(), t[i].end(), c + 1) - t[i].begin());
-		return ans - c + 1;
+		for(; x; x -= lowbit(x)) ans += t[x];
+		return ans;
 	}
-	
 }T;
 
-void solve2(){
-	for(int i = 1; i <= n; i++){
-		T.f[i] = i;
-		while(T.f[i] >= 2 and (arr[i] >= arr[T.f[i] - 1]))
-			T.f[i] = T.f[T.f[i] - 1];
-		T.add(i); sort(T.t[i].begin(), T.t[i].end());
+struct Tq{
+	int l; int r;	
+}que[MAXN];
+int p[MAXN] = { 0 };
+int b[MAXN] = { 0 };           // 处理左端点时 b[i] = h[que[i].l]
+int h[MAXN] = { 0 };           // 处理左端点时 h[que[i].l] = i
+int ans[MAXN] = { 0 };
+void solve(){
+	stk[++top] = 1; p[1] = 1; T.add(1, 1); 
+	for(int i = 2; i <= n; i++){
+		while(stk[top] and (arr[i] >= arr[stk[top]])) top--;
+		p[i] = stk[top] + 1; stk[++top] = i;
 	}
-	while(q--){ int l = in; int r = in; cout << (T.query(l, r)) << '\n'; }
+	for(int i = 1; i <= q; i++) { que[i].l = in; que[i].r = in; }            // 把询问离线下来 
+	for(int i = 1; i <= q; i++) b[i] = h[que[i].l], h[que[i].l] = i;
+	for(int i = 1; i <= n; i++){
+		for(int j = h[i]; j; j = b[j]) ans[j] -= T.query(que[j].l);
+		T.add(p[i], 1);
+	}
+	T.clear(); memset(h, 0, sizeof(h));
+	for(int i = 1; i <= q; i++) b[i] = h[que[i].r], h[que[i].r] = i;
+	for(int i = 1; i <= n; i++){
+		T.add(p[i], 1);
+		for(int j = h[i]; j; j = b[j]) ans[j] += T.query(que[j].l);
+	}
+	for(int i = 1; i <= q; i++) cout << ans[i] + 1 << '\n';
 }
 
 int main(){
 	n = in;  q = in;
-	bool flaga = 1; bool flagb = 1;
-	for(int i = 1; i <= n; i++) if((arr[i].a = in) != i) flaga = 0;
-	for(int i = 1; i <= n; i++) if((arr[i].b = in) != (n - i + 1)) flagb = 0;
-	if(n <= 5000) solve1();
-	else solve2();
+	for(int i = 1; i <= n; i++) arr[i].a = in;
+	for(int i = 1; i <= n; i++) arr[i].b = in;
+	solve();
 	return 0;
 }
