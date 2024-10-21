95009d504ebd3a76975296f5a52f68d7fabd5412
AlexWei061 <aty.wei@foxmail.com>
2024-10-21 21:24:56 +0800

    add correction

---
+++ contest/WuHan核桃杯/正式赛/cor/d.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAXN 100100 
+
+int n = 0;
+int a[MAXN] = { 0 };
+int b[MAXN] = { 0 };
+
+int main(){
+	cin >> n;
+	for(int i = 1; i <= n; i++) cin >> a[i];
+	for(int i = 2; i <= n; i++) b[i] = a[i] - a[i - 1];
+	int ans = 0;
+	for(int i = 2; i <= n; i++) ans = max(ans, b[i]);
+	cout << ans << '\n';
+	return 0;
+} 
